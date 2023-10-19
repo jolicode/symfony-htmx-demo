aec109ecd60b6c653ba8fb47e3b99c388c964d90
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2023-10-19 11:07:10 +0200

    Setting up the project

---
+++ src/Controller/SpaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SpaController extends AbstractController
+{
+    #[Route('/spa', name: 'app_spa')]
+    public function index(): Response
+    {
+        return $this->render('spa/index.html.twig', [
+            'controller_name' => 'SpaController',
+        ]);
+    }
+}
