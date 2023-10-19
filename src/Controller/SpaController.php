cd4666386a74cfa14bf87bcff9b91fbd1a81f4d2
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2023-10-19 11:27:07 +0200

    Navigating

--- src/Controller/SpaController.php
+++ src/Controller/SpaController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SpaController extends AbstractController
 {
-    #[Route('/spa', name: 'app_spa')]
+    #[Route('/', name: 'app')]
     public function index(): Response
     {
         return $this->render('spa/index.html.twig', [
             'controller_name' => 'SpaController',
         ]);
+    }
+
+    #[Route('/new-page', name: 'app_new_page')]
+    public function newPage(): Response
+    {
+        return $this->render('spa/new_page.html.twig');
     }
 }
