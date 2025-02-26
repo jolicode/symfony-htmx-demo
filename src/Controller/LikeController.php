4c4ef4e9b39f3090d2d946095f2794e4e6457ed1
Damien Alexandre <dalexandre@jolicode.com>
2025-02-26 18:11:38 +0100

    Add a simple LIKE toggle demo with CSS transition

---
+++ src/Controller/LikeController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LikeController extends AbstractController
+{
+    #[Route('/like')]
+    public function index(Request $request): Response
+    {
+        return $this->render('like/index.html.twig', [
+            'isLiked' => $request->getSession()->get('isLiked', false),
+        ]);
+    }
+
+    #[Route('/like/toggle', methods: ['POST'])]
+    public function toggle(Request $request): Response
+    {
+        $request->getSession()->set(
+            'isLiked',
+            !$request->getSession()->get('isLiked', false)
+        );
+
+        return $this->renderBlock('like/index.html.twig', 'like', [
+            'isLiked' => $request->getSession()->get('isLiked', false),
+        ]);
+    }
+}
